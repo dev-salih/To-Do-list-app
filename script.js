8e07841dfd7853c3ca3156a3e7e58025c55c13d5
dev-salih <salihmu79@gmail.com>
2026-04-17 11:28:48 +0300

    lists saved in localstorage

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ function addTask(){
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
-    // inputBox.value = "";
+    inputBox.value = "";
+    saveData();
 }
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
     } 
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
