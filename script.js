48bfa21be5d19f4e6b0e725fb20596b7d3c53c03
dev-salih <salihmu79@gmail.com>
2026-04-17 11:30:04 +0300

    saved data displayed in browser

--- script.js
+++ script.js
@@ -28,3 +28,7 @@ listContainer.addEventListener("click", function(e){
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
